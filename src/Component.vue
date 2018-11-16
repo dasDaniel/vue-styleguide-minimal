8254f2cd0d30b2a6206d79338303cedb88872cdc
daniel.poda@gmail.com <Daniel.Poda@gmail.com>
2018-11-15 21:27:47 -0700

    initial commit

---
+++ src/Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Component",
+  data() {
+    return {
+      msg: "Hello",
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
